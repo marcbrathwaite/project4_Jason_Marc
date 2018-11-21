bdc019ab92d1f96e7c71aa71535894929f57276e
Jason <j.mortfield44@gmail.com>
2018-11-21 18:43:44 -0500

    Created functionality to grab food item names based on search query, added some basic markup

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,69 @@
+const foodApp = {};
+
+foodApp.init = function(){
+    foodApp.events();
+}
+
+foodApp.events = function(){
+    // Get search term and value of search type=nutritional. Save value in variable
+    $('#submitBtn').on('click', function(event){
+        event.preventDefault();
+        // Clears display
+        foodApp.clearDOM();
+        // Retrieves value of search box
+        const searchTerm = $('#searchField').val();
+        console.log(searchTerm);
+        // Clear search field
+        $('#searchField').val('');
+        //Create promise for food item search
+        const promise = foodApp.getFoodItems(searchTerm);
+        foodApp.makeAPIRequest(promise, foodApp.displayFoodItem);
+    });
+};
+
+foodApp.apiKeyUSDA = 'TqiCp1WcVXZzchDeT3DL0M8mk2OrOirzhkXgTrwa';
+
+// Create ajax for USDA(?) foodItem API and pass search into q in API
+
+foodApp.getFoodItems = function(search) {
+    return $.ajax({
+        url: 'http://api.nal.usda.gov/ndb/',
+        datatype: 'json',
+        method: 'GET',
+        data: {
+            api_key: foodApp.apiKeyUSDA,
+            format: 'json',
+            q: search, // Get from search query
+            max: 50,
+            nutrients: 'y'
+        }
+    });
+};
+
+foodApp.makeAPIRequest = function(promise, fn){
+    $.when(promise).then((res) => {
+        fn(res);
+    });
+};
+
+foodApp.displayFoodItem = function(res) {
+    const foodItemArray = res.list.item;
+    let foodItemHTML = '';
+    foodItemArray.forEach(function(elm){
+        // const name = elm.name.replace(/(GTIN:\s+\d+)|(UPC:\s+\d+) /ig,'');
+        const name = elm.name.replace(/GTIN:\s*\d*|UPC:\s*\d*/ig,'');
+        foodItemHTML += `<li id=${elm.ndbno}>${name}</li>`;
+    });
+    $('#resultsList').append(foodItemHTML);
+}
+
+
 // User enters search query and selects nutritional info
 
     // Erase any current html in aside list and main section
-
-    // Get search term and value of search type=nutritional. Save value in variable
-
-    // Create ajax for USDA(?) foodItem API and pass search into q in API
+    foodApp.clearDOM = function(){
+        $('#resultsList').empty();
+    }
 
     // Create preloader while user waits for results
 
@@ -23,6 +82,9 @@
     // Take results from nutrients and add to DOM
 
 // User recieves picture from Unsplash(?) and nutritional info 
+
+
+
 
 
 
@@ -51,4 +113,6 @@
 // Recieves picture from not Unsplash, ingredient list, possible cooking instructions
 
 
-
+$(function(){
+    foodApp.init();
+})
